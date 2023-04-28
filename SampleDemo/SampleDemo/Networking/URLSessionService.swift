27b7bbb2d7ce7da308ffea33daa06b6733b7c4dd
Nouman Gul <Noumanguljunejo@gmail.com>
2023-04-28 20:22:32 +0500

    Changing Codable To Decodable

--- SampleDemo/SampleDemo/Networking/URLSessionService.swift
+++ SampleDemo/SampleDemo/Networking/URLSessionService.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct URLSessionService {
     
-    static func fetchData<T: Codable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+    static func fetchData<T: Decodable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
                 completion(.failure(error!))
@@ -17,6 +17,7 @@ struct URLSessionService {
             }
             do {
                 let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let decodedData = try decoder.decode(T.self, from: data)
                 completion(.success(decodedData))
             } catch let error {
@@ -27,7 +28,7 @@ struct URLSessionService {
     }
 
     
-    static func fetchData<T: Codable>(url: URL) async throws -> (Result<T, Error>) {
+    static func fetchData<T: Decodable>(url: URL) async throws -> (Result<T, Error>) {
         let (data, response) = try await URLSession.shared.data(for: URLRequest(url: url))
         guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
             throw NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil)
